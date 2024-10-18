9577ab75b31fd9a4d37202c3e35cfd7ba3851eb0
xden78 <venera.opt@gmail.com>
2024-10-18 20:09:50 +0300

    attempt 3

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -4,19 +4,24 @@ public class SalaryCalculator {
     public void calculateSalary(int salary) {
         //  Basic Salary <= 10000: taxes - 15%. 10000 < Basic Salary <= 20000:
         //  taxes - 18% Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
-        int salaryAfterTaxes;
+        if (salary < 0) {
+            System.out.println("wrong input!");
+            return;
+        }
+
+        float salaryAfterTaxes;
+
         if (salary <= 10000) {
-            salaryAfterTaxes = salary - ((salary * 15) / 100);
+            salaryAfterTaxes = salary - ((float)(salary * 15) / 100);
             System.out.println(salaryAfterTaxes);
         }
         if (salary > 10000 && salary <= 20000) {
-            salaryAfterTaxes = salary - ((salary * 18) / 100);
+            salaryAfterTaxes = salary - ((float)(salary * 18) / 100);
             System.out.println(salaryAfterTaxes);
         }
         if (salary > 20000) {
-            salaryAfterTaxes = salary - ((salary * 20) / 100);
+            salaryAfterTaxes = salary - ((float)(salary * 20) / 100);
             System.out.println(salaryAfterTaxes);
         }
-        else if (salary < 0) System.out.println("wrong input!");
     }
 }
